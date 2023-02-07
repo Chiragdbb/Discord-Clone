5ac453f6d23d20d393cf2c4a9fc241a2adf3b28f
Chiragdbb <schirag414@gmail.com>
2023-02-07 19:59:24 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Whitney": ["Whitney"],
+        "Ginto": ["Ginto"],
+        "ggSans": ["ggSans"],
+        "GintoNord": ["GintoNord"]
+      },
+      colors:{
+        disBlack: "#23272a",
+        disBlue: "#404eed",
+      },
+    },
+  },
+  plugins: [],
+}
